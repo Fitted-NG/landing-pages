2d9052228058359799c553ea0c8c357b50387d18
joseph emmanuel kayode (iemarjay) <ejoseph@wikimedia.org>
2023-09-15 11:50:10 +0400

    add social links

--- src/components/reusable/FooterSocials.tsx
+++ src/components/reusable/FooterSocials.tsx
@@ -9,7 +9,7 @@ export default function FooterSocials() {
   return (
     <div className="flex justify-center items-center gap-3">
       <a
-        href="#"
+        href="https://www.facebook.com/fitted.ng/"
         target="_blank"
         rel="noreferrer"
         className="w-10 h-10 lg:w-12 lg:h-12 border border-[#535D69] rounded-full flex justify-center item-center"
@@ -23,7 +23,7 @@ export default function FooterSocials() {
       </a>
 
       <a
-        href="#"
+        href="https://www.youtube.com/channel/UCHcrTqV2Z2L391s_HGPb2Og"
         target="_blank"
         rel="noreferrer"
         className="w-10 h-10 lg:w-12 lg:h-12 border border-[#535D69] rounded-full flex justify-center item-center"
@@ -37,7 +37,7 @@ export default function FooterSocials() {
       </a>
 
       <a
-        href="#"
+        href="https://www.instagram.com/fitted.ng/"
         target="_blank"
         rel="noreferrer"
         className="w-10 h-10 lg:w-12 lg:h-12 border border-[#535D69] rounded-full flex justify-center item-center"
