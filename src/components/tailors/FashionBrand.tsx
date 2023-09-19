7b3da367d7c6d0501e01bf77bca1821c097ab582
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-09-19 21:12:58 +0100

    fashion brand text changed to xl

--- src/components/tailors/FashionBrand.tsx
+++ src/components/tailors/FashionBrand.tsx
@@ -8,7 +8,7 @@ export default function FashionBrand() {
       <div className="container mx-auto flex justify-between items-center max-lg:flex-col gap-10 w-full">
         <div className="flex flex-1 flex-col lg:items-start sm:items-center">
           <h2 className="font-kotori-rose text-[#152C5B] font-bold lg:text-4xl text-xl lg:w-[500px]">Are you a fashion brand getting your products out?</h2>
-          <p className="font-avenir text-[#4F4F4F] font-light mt-4 mb-4 sm:max-w-[370px] lg:w-[514px] text-xl">
+          <p className="font-avenir text-[#4F4F4F] font-light mt-4 mb-4 sm:max-w-[370px] lg:w-[514px] text-lg">
              Take your fashion business to the next level by focusing on what matters. Scale the production of your fashion pieces and designs without the headache of Tailor recruitment.
           </p>
           <Button type="blue">Join as fashion brand</Button>
