683b94a2765419f97e9e85a741178c8d5e9cc919
TPriince <tobiloba110@gmail.com>
2023-09-22 10:47:25 +0100

    fix: Resove merge conflict

--- src/components/tailors/Header.tsx
+++ src/components/tailors/Header.tsx
@@ -1,18 +1,15 @@
 import Image from "next/image";
 import Logo from "/public/header-logo-light.png";
 import HeaderButtons from "./HeaderButton";
-import Menu from "./Menu";
 
 export default function Header() {
   return (
-    <header className="max-w-[1440px] mx-auto sm:px-16 px-6 py-12">
+    <header className="max-w-[1440px] mx-auto sm:px-16 px-6 sm:py-12 py-12">
       <div className="flex justify-between items-center">
         <div className="w-[110px] h-10 md:w-[175px] md:h-[62px] flex justify-center items-center">
           <Image src={Logo} alt="logo" loading="lazy" />
         </div>
       </div>
-
-      {/* <Menu />  */}
 
       <div>
         <HeaderButtons />
