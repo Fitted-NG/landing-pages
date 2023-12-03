2d609973e06fb9366264ddd170a5eb8d069dec2e
TPriince <tobiloba110@gmail.com>
2023-12-03 19:38:29 +0100

    feat: Add Purple Puffy Dress product

--- src/components/magic/product/ProductDetails.tsx
+++ src/components/magic/product/ProductDetails.tsx
@@ -16,22 +16,39 @@ export default function ProductDetails({
   return (
     <section className="bg-white">
       <div className="w-full h-[400px] flex gap-4 overflow-x-scroll no-scrollbar">
-        {product.images.map((img, index) => (
-          <div key={index} className="w-full min-w-[350px] h-full relative">
-            <Image
-              src={img}
-              alt={product.name}
-              fill={true}
-              priority
-              className="object-cover object-center"
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
-            <div className="absolute left-4 bottom-[18px] flex items-center gap-1 py-2 px-3 bg-[#ffffffe5]/90 rounded-[40px] backdrop-blur-md">
-              <Image src={bagIcon} alt="bag icon" />
-              <span className="text-[#1D2939] text-sm">Shop Look</span>
+        {product.images.length > 0 ? (
+          product.images.map((img, index) => (
+            <div key={index} className="min-w-[350px] h-full relative">
+              <Image
+                src={img}
+                alt={product.name}
+                fill={true}
+                priority
+                className="object-cover object-center"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+              <div className="absolute left-4 bottom-[18px] flex items-center gap-1 py-2 px-3 bg-[#ffffffe5]/90 rounded-[40px] backdrop-blur-md">
+                <Image src={bagIcon} alt="bag icon" />
+                <span className="text-[#1D2939] text-sm">Shop Look</span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <>
+            <div className="min-w-[320px] h-full relative bg-pink-fitted/50 flex justify-center items-center">
+              <p className="text-white text-center">Image yet to be uploaded</p>
+            </div>
+            <div className="min-w-[320px] h-full relative bg-pink-fitted/50 flex justify-center items-center">
+              <p className="text-white text-center">Image yet to be uploaded</p>
+            </div>
+            <div className="min-w-[320px] h-full relative bg-pink-fitted/50 flex justify-center items-center">
+              <p className="text-white text-center">Image yet to be uploaded</p>
+            </div>
+            <div className="min-w-[320px] h-full relative bg-pink-fitted/50 flex justify-center items-center">
+              <p className="text-white text-center">Image yet to be uploaded</p>
+            </div>
+          </>
+        )}
       </div>
 
       <div className="p-4 flex flex-col gap-5">
